e522795d2a98f492a97701af5bf5572c0c29b60e
Nick Landas <nick.landas@gmail.com>
2023-02-16 21:44:11 -0600

    retooled to include Auth0

---
+++ src/components/hero-banner.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export const HeroBanner = () => {
+  const logo = "https://gistcdn.githack.com/nick-landas/b621710e149e6cf2f68bb21755d6e470/raw/10abe37899c9b2633923c1a94550aca5d98c0d7f/la.svg";
+
+  return (
+    <div className="hero-banner hero-banner--mandarine-orange">
+      <div className="hero-banner__logo">
+        <img className="hero-banner__image" src={logo} alt="LA logo" />
+      </div>
+      <h1 className="hero-banner__headline">Hey, Y'all!</h1>
+      <p className="hero-banner__description">
+        This is I Luv LA (Lower Alabama) Chat.
+      </p>
+      <a
+        id="code-sample-link"
+        target="_blank"
+        rel="noopener noreferrer"
+        href="/"
+        className="button button--primary"
+      >
+        Sign yourself up →
+      </a>
+    </div>
+  );
+};
---
+++ src/pages/home-page.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { HeroBanner } from "../components/hero-banner";
+import { PageLayout } from "../components/page-layout";
+
+export const HomePage = () => (
+  <PageLayout>
+    <HeroBanner />
+  </PageLayout>
+);
